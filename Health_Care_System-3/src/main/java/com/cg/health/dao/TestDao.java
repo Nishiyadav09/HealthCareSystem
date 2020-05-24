07b4e607f850f822da0748745d7067a31e78a25a
Nishiyadav09 <46625144+Nishiyadav09@users.noreply.github.com>
2020-05-24 13:05:13 +0530

    Update TestDao.java

--- Health_Care_System-3/src/main/java/com/cg/health/dao/TestDao.java
+++ Health_Care_System-3/src/main/java/com/cg/health/dao/TestDao.java
@@ -1,6 +1,15 @@
 package com.cg.health.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.cg.health.entity.Test;
+/*****************************************************************************************************
+ * @author        Nishi Yadav
+ * Description    It is a TestDao interface typically which extends the JpaRepository 
+ *                which implements the method. 
+ *                
+ * Version        1.0
+ * Created Date   06-MAY-2020 
+ *
+ ****************************************************************************************************/
 public interface TestDao extends JpaRepository<Test, Integer> {
 
 }
