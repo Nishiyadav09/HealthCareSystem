7e6290be49df62a796a26d51bb48797886041a5b
Nishiyadav09 <46625144+Nishiyadav09@users.noreply.github.com>
2020-05-24 13:07:21 +0530

    Update TestAdvice.java

--- Health_Care_System-3/src/main/java/com/cg/health/exception/TestAdvice.java
+++ Health_Care_System-3/src/main/java/com/cg/health/exception/TestAdvice.java
@@ -4,7 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+/*****************************************************************************************************
+ * @author        Nishi Yadav
+ * Description    It is a ControllerAdvice class  which uses the ExceptionHandler annotation
+ *                to hanlde the custom exception. 
+ *               
+ *                
+ * Version        1.0
+ * Created Date   13-MAY-2020 
+ *
+ ****************************************************************************************************/
 @RestControllerAdvice
 public class TestAdvice {
 	@ExceptionHandler(value = {TestsException.class})
